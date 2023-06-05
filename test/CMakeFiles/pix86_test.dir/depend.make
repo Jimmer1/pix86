8164a1437c8ecd68959c8f30bc15d6c0e0dd19a0
Jimmer1 <jr195698@gmail.com>
2023-06-05 22:43:25 +0100

    cleanup

--- test/CMakeFiles/pix86_test.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for pix86_test.
-# This may be replaced when dependencies are built.
--- test_pix86.cc
+++
@@ -1,8 +0,0 @@
-#include "test.hh"
-
-int main() {
-    test_decoder();
-    test_generic_reference();
-    test_fpu();
-    test_util();
-}
